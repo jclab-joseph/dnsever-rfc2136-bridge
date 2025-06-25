5c2790d65aff17fcf33b155160e604e44641f3d3
Joseph Lee <joseph@jc-lab.net>
2025-06-25 12:32:52 +0900

    feat: udp server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,6 +7,7 @@ import (
 	"gopkg.in/yaml.v3"
 	"log"
 	"os"
+	"sync"
 
 	"github.com/miekg/dns"
 )
@@ -17,6 +18,17 @@ func getEnvOrDefault(name string, def string) string {
 		value = def
 	}
 	return value
+}
+
+type App struct {
+	handler   *dnshandler.Handler
+	tcpServer *dns.Server
+	udpServer *dns.Server
+}
+
+func (a *App) addTsigSecret(name string, secret string) {
+	a.tcpServer.TsigSecret[name] = secret
+	a.udpServer.TsigSecret[name] = secret
 }
 
 func main() {
@@ -39,21 +51,45 @@ func main() {
 	handler := &dnshandler.Handler{}
 	handler.ApplyAuthFile(&authFile)
 
-	server := &dns.Server{
-		Addr:          listen,
-		Net:           "tcp",
-		TsigSecret:    make(map[string]string),
-		Handler:       handler,
-		MsgAcceptFunc: handler.MsgAccept,
+	app := &App{
+		handler: handler,
+		tcpServer: &dns.Server{
+			Addr:          listen,
+			Net:           "tcp",
+			TsigSecret:    make(map[string]string),
+			Handler:       handler,
+			MsgAcceptFunc: handler.MsgAccept,
+		},
+		udpServer: &dns.Server{
+			Addr:          listen,
+			Net:           "udp",
+			TsigSecret:    make(map[string]string),
+			Handler:       handler,
+			MsgAcceptFunc: handler.MsgAccept,
+		},
 	}
 	for _, domain := range authFile.Domains {
 		for _, auth := range domain.Tsig {
-			server.TsigSecret[auth.Name] = auth.Secret
+			app.addTsigSecret(auth.Name, auth.Secret)
 		}
 	}
 
+	var wg sync.WaitGroup
+
 	log.Printf("Starting DNS UPDATE server on %s", listen)
-	if err := server.ListenAndServe(); err != nil {
-		log.Fatalf("Failed to start server: %s", err.Error())
-	}
+
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		if err := app.tcpServer.ListenAndServe(); err != nil {
+			log.Fatalf("Failed to start tcp server: %s", err.Error())
+		}
+	}()
+	go func() {
+		defer wg.Done()
+		if err := app.udpServer.ListenAndServe(); err != nil {
+			log.Fatalf("Failed to start udp server: %s", err.Error())
+		}
+	}()
+	wg.Wait()
 }
